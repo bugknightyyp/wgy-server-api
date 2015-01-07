89674865fac27f2adbd52ba7c8080918f2d17e1c
bugknightyyp <bugknightyyp@gmail.com>
2015-01-07 20:39:25 +0800

    init

--- SUMMARY.md
+++
@@ -1,5 +0,0 @@
-# Summary
-
-* [前言](content/preface.md)
-* [Chapter 2](chapter2.md)
-* [Chapter 3](chapter3.md)
--- togh.sh
+++
@@ -1,6 +0,0 @@
-gitbook build .
-cp -rf _book/* ~/tmp/gitbook
-cd ~/tmp/gitbook
-git add .
-git ci -m 'pub'
-git push origin gh-pages
